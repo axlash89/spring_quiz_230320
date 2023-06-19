a05cf18f0504c76474520c8485d82a082ac14d1d
axlash89 <axlash7@gmail.com>
2023-06-19 14:55:59 +0900

    강남 할리스

---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,35 @@
+package com.quiz.lesson03.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.dao.RealEstateMapper;
+import com.quiz.lesson03.domain.RealEstate;
+
+@Service
+public class RealEstateBO {
+	
+	// input : id
+	// output : RealEstate  =>  컨트롤러한테
+	
+	@Autowired
+	private RealEstateMapper realEstateMapper;
+	
+	public RealEstate getRealEstateById(int id) {
+		return realEstateMapper.selectRealEstateById(id);
+	}
+	
+	// input : rentPrice
+	// output : List<RealEstate>
+	
+	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice) {
+		return realEstateMapper.selectRealEstateListByRentPrice(rentPrice);
+	}
+	
+	public List<RealEstate> getRealEstateListByAreaPrice(int area, int price) {
+		return realEstateMapper.selectRealEstateListByAreaPrice(area, price);
+	}
+
+}
